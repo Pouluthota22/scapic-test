29dfcb5f254f45d3e0c1558065716f57f1a3382a
Poulu Thota <pouluthota@gmail.com>
2019-01-11 12:40:53 +0530

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : 'XXXXXXXXXX', // your App ID
+        'clientSecret'  : 'XXXXXXXXX', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    // 'twitterAuth' : {
+    //     'consumerKey'       : 'your-consumer-key-here',
+    //     'consumerSecret'    : 'your-client-secret-here',
+    //     'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    // },
+
+    'googleAuth' : {
+        'clientID'      : 'XXXXXXXXXX',//your-secret-clientID-here'
+        'clientSecret'  : 'XXXXXXXXXXX',//your-client-secret-here
+        'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://admin:admin123@ds153304.mlab.com:53304/assessments' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
